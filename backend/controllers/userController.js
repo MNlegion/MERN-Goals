e17d96e337ba0806976f38fa7bd1c666a2a0de5f
MNlegion <rkreuser30@gmail.com>
2023-12-19 19:50:14 -0500

    register user functionality in userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,44 @@ const User = require("../models/userModel");
 // Notes: @route POST /api/users
 // Notes: Public route
 const registerUser = asyncHandler(async (req, res) => {
-    res.json({message: "Register User"});
+
+    const {name, email, password} = req.body;
+    
+    if (!name || !email || !password) {
+        res.status(400);
+        throw new Error("Please enter all fields");
+    }
+
+    // Check if user already exists
+    const userExists = await User.findOne({email});
+
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create new user
+    const newUser = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    });
+
+    if(newUser) {
+        res.status(201).json({
+            _id: newUser._id,
+            name: newUser.name,
+            email: newUser.email,
+            // token: generateToken(newUser._id),
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid user credentials");
+    }
 });
 
 // Purpose: Authenticate a User & get a token
