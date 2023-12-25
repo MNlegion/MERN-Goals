58f9b087dab8c79ffb937f59bc63397b47383615
MNlegion <rkreuser30@gmail.com>
2023-12-24 22:08:12 -0500

    page title name

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -17,7 +17,7 @@ function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <Link to="/">GoalSetter</Link>
+        <Link to="/">Goal Setter by MNlegion</Link>
       </div>
       <ul>
         {user ? (
